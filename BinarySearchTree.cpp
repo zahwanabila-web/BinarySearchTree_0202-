45f278e102e69ffba1c06882a3800a5944367b23
ZahwaNabila <zahwanabila434@gmail.com>
2026-06-09 11:32:44 +0700

    commit12

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -122,3 +122,29 @@ public:
     }
 };
 
+int main()
+    {
+        binarytree obj;
+
+        while (true)
+        {
+            cout << "\nmenu" << endl;
+            cout << "1. implement insert operation" << endl;
+            cout << "2. implement inorder traversal" << endl;
+            cout << "3. implement preorder traversal" << endl;
+            cout << "4. implement postorder traversal" << endl;
+            cout << "5. exit" << endl;
+            cout << "\nenter your choice: " << endl;
+
+            char ch;
+            cin >> ch;
+
+            cout << endl;
+
+            switch (ch)
+        {
+            
+        }
+    }
+    
+}
