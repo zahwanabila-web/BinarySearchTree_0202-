cf8e14b19924a171baa7f3aa3457a3cb5b232bfb
ZahwaNabila <zahwanabila434@gmail.com>
2026-06-09 11:26:29 +0700

    commit8

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -62,5 +62,15 @@ public:
         currentnode = root;
         parent = NULL;
 
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+
     }
 };
