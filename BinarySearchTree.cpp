dfa2290667c92fc51725b472fb4178686097f261
ZahwaNabila <zahwanabila434@gmail.com>
2026-06-09 11:22:36 +0700

    commit5

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -27,4 +27,26 @@ public:
     {
         root = NULL;
     }
+
+    void insert(string element)
+    {
+        node *newnode = new node(element, NULL, NULL);
+        
+        newnode->info = element;
+        newnode->leftchild = NULL;
+        newnode->rightchild = NULL;
+
+        node *parent = NULL;
+        node *currentnode = NULL;
+
+        search(element, parent, currentnode);
+
+        if (parent == NULL)
+        {
+            root = newnode;
+            return;
+        }
+
+       
+    }
 };
